197a24f4e5f60b4ddf398b013d8b6e63ec442d1b
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-10-28 01:01:35 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -13,7 +13,7 @@ from curbd_dataset import CURBD3RegionsDataset
 from curbd_training import train_with_curbd, make_eval_prediction
 from figures_script import complete_report
 from ts_dataset import TSDataset
-from util import model_summary
+from util import model_summary, TBPTTHook
 
 if __name__ == '__main__':
     number_units = -1
@@ -40,8 +40,11 @@ if __name__ == '__main__':
         dt_data=dt,
         re_hh=False,
         # filename="spikes.npy",
-        filename="dff_matrix.npy",
+        filename="dff_matrix_N1644_T1975.npy",
+        # filename="dff_matrix_N1019_T1975.npy",
+        # filename="dff_matrix_N600_T1974.npy",
         # smoothing_sigma=1,
+        # smoothing_sigma=2*3.57,
         smoothing_sigma=2*3.57,
         normalize=True,
         normalize_mth="min_max",
@@ -78,51 +81,80 @@ if __name__ == '__main__':
     if layer.force_dale_law:
         layer.get_forward_sign_parameter().requires_grad = False
         layer.get_recurrent_sign_parameter().requires_grad = True
-    dale_str = f"dale" if layer.force_dale_law else f""
+    dale_str = f"_dale" if layer.force_dale_law else f""
     model = nt.SequentialRNN(
         layers=[layer],
-        device=torch.device("cpu"),
-        checkpoint_folder=f"data/checkpoints_"
-                          f"{dataset.filename[:-4]}_"
-                          f"{dataset.normalize_mth}{nbu_str}_"
-                          f"{layer.name}_"
+        device=torch.device("cpu" if layer.force_dale_law else "cuda"),
+        # device=torch.device("cuda"),
+        checkpoint_folder=f"data/{dataset.filename[:-4]}/checkpoints"
+                          f"_{dataset.normalize_mth}{nbu_str}"
+                          f"_{layer.name}"
                           f"{dale_str}"
-                          f"_mixtr",
+                          f"_sig{str(dataset.sigma).replace('.', '-')}"
+                          # f"_mixtr",
     ).build()
     print(f"Model:\n{model},\nCheckpoint folder: {model.checkpoint_folder}")
     print(model_summary(model, (1, dataset.n_units)))
     model, history, trainer = train_with_curbd(
         model, dataset,
-        force_overwrite=False,
-        use_lr_scheduler=False,
-        n_iterations=100,
+        force_overwrite=True,
+        n_iterations=1_000 if model.device.type == "cpu" else 300,
         lr=1.0,
-        # learning_algorithm=nt.TBPTT(
-        #     backward_time_steps=1,
-        #     optim_time_steps=1,
-        #     default_optimizer_cls=torch.optim.AdamW,
-        #     params_lr=2e-4,
-        # ),
-        lr_schedule_start=0.0,
-        callbacks=[
-            nt.TBPTT(
-                # params=[layer.get_recurrent_sign_parameter()],
-                params=[
-                    p for p in
-                    nt.utils.filter_parameters(list(model.parameters()), requires_grad=True)
-                    if p is not layer.get_recurrent_weights_parameter()
-                ],
-                backward_time_steps=1,
-                optim_time_steps=1,
-                default_optimizer_cls=torch.optim.AdamW,
-                params_lr=2e-4,
-            ),
-            # nt.BPTT(
-            #     params=nt.utils.filter_parameters(list(model.parameters()), requires_grad=True),
-            #     default_optimizer_cls=torch.optim.AdamW,
-            #     params_lr=2e-4,
-            # )
-        ] if layer.force_dale_law else [],
+        **{
+            "learning_algorithm":
+                TBPTTHook(
+                    params=nt.utils.filter_parameters(list(model.parameters()), requires_grad=True),
+                    backward_time_steps=1,
+                    optim_time_steps=1,
+                    default_optimizer_cls=torch.optim.AdamW,
+                    params_lr=1e-2,
+                    grad_norm_clip_value=0.5,
+                ) if layer.force_dale_law else None,
+            # "callbacks": [
+            #         TBPTTHook(
+            #             # params=[layer.get_recurrent_sign_parameter()],
+            #             params=[
+            #                 p for p in
+            #                 nt.utils.filter_parameters(list(model.parameters()), requires_grad=True)
+            #                 if p is not layer.get_recurrent_weights_parameter()
+            #             ],
+            #             backward_time_steps=1,
+            #             optim_time_steps=1,
+            #             default_optimizer_cls=torch.optim.AdamW,
+            #             params_lr=1e-2,
+            #         )
+            #     ],  # if not layer.force_dale_law else [],
+            "use_lr_scheduler": layer.force_dale_law,
+            "lr_schedule_start": 0.0,
+        },
+    )
+    # model, history, trainer = train_with_curbd(
+    #     model, dataset,
+    #     force_overwrite=True,
+    #     n_iterations=100 if model.device.type == "cpu" else 300,
+    #     lr=1.0,
+    #     **{
+    #         "learning_algorithm":
+    #             nt.TBPTT(
+    #                 params=nt.utils.filter_parameters(list(model.parameters()), requires_grad=True),
+    #                 backward_time_steps=1,
+    #                 optim_time_steps=1,
+    #                 default_optimizer_cls=torch.optim.AdamW,
+    #                 params_lr=1e-2,
+    #                 grad_norm_clip_value=0.5,
+    #             ),
+    #         "use_lr_scheduler": layer.force_dale_law,
+    #         "lr_schedule_start": 0.0,
+    #     },
+    # )
+    eval_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    y_pred, y_target = make_eval_prediction(model, dataset, device=eval_device)
+    wc_viz_pred, wc_viz_target = complete_report(model, y_pred=y_pred, y_target=y_target)
+    model, history, trainer = train_with_curbd(
+        model, dataset,
+        force_overwrite=False,
+        n_iterations=300 if model.device.type == "cpu" else 900,
+        lr=1.0,
     )
     eval_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     y_pred, y_target = make_eval_prediction(model, dataset, device=eval_device)

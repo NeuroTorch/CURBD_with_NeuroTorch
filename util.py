0b684eef8fe8a9c354997ae3780cefb50a3d72bb
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-11-01 13:22:38 -0400

    Update util.py

--- util.py
+++ util.py
@@ -1,13 +1,13 @@
 import os
 from collections import OrderedDict
-from typing import List, Tuple
+from typing import List, Optional, Iterable
 
 import numpy as np
 import torch
 import neurotorch as nt
-from neurotorch import RLS
+from neurotorch.learning_algorithms.learning_algorithm import LearningAlgorithm
 from neurotorch.modules import BaseModel
-from neurotorch.utils import unpack_out_hh, recursive_detach_, list_insert_replace_at, ConnectivityConvention
+from neurotorch.utils import unpack_out_hh, recursive_detach_, list_insert_replace_at
 from torch.utils.hooks import RemovableHandle
 
 
@@ -66,6 +66,10 @@ def model_summary(
     """
     if input_size is None:
         input_size = (1, model.input_size)
+
+    # create properties
+    summary = OrderedDict()
+    hooks = []
 
     def register_hook(module):
 
@@ -132,17 +136,11 @@ def model_summary(
 
     # batch_size of 2 for batchnorm
     x = [torch.rand(2, *in_size).type(dtype) for in_size in input_size]
-    # print(type(x[0]))
-
-    # create properties
-    summary = OrderedDict()
-    hooks = []
 
     # register hook
     model.apply(register_hook)
 
     # make a forward pass
-    # print(x.shape)
     model(*x)
 
     # remove these hooks
@@ -245,38 +243,150 @@ class TBPTTHook(nt.TBPTT):
         return
 
 
-class RLSHook(RLS, TBPTTHook):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class SaveObjsCallback(nt.callbacks.BaseCallback):
+    DEFAULT_PRIORITY = nt.callbacks.BaseCallback.DEFAULT_LOW_PRIORITY
 
-    def _output_hook(self, module, args, kwargs, output) -> None:
-        t, forecasting = kwargs.get("t", None), kwargs.get("forecasting", False)
-        if t is None:
-            return
+    def __init__(
+            self,
+            model,
+            dataset,
+            pkl_sub_folder="pickles",
+            repr_sub_folder="infos",
+            eval_device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+    ):
+        super().__init__()
+        self.model = model
+        self.dataset = dataset
+        self.pkl_sub_folder = pkl_sub_folder
+        self.repr_sub_folder = repr_sub_folder
+        self.eval_device = eval_device
 
-        layer_name = module.name
-        out_tensor, hh = unpack_out_hh(output)
-        list_insert_replace_at(self._layers_buffer[layer_name], t % self.backward_time_steps, out_tensor)
-        if len(self._layers_buffer[layer_name]) == self.backward_time_steps:
-            self._backward_at_t(t, self.backward_time_steps, layer_name)
-            if self.strategy in ["grad", "jacobian", "scaled_jacobian"]:
-                output = recursive_detach_(output)
+    def to_pickle(self):
+        import pickle
+        from curbd_training import make_eval_prediction
+        from figures_script import complete_report
 
-    @staticmethod
-    def curbd_step_method(
-            inv_corr: torch.Tensor,
-            post_activation: torch.Tensor,
-            error: torch.Tensor,
-            connectivity_convention: ConnectivityConvention = ConnectivityConvention.ItoJ,
-            **kwargs
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        delta_weights, delta_inv_corr = RLS.curbd_step_method(
-            inv_corr=inv_corr,
-            post_activation=post_activation,
-            error=error,
-            connectivity_convention=connectivity_convention,
-            **kwargs
+        folder = os.path.join(self.model.checkpoint_folder, self.pkl_sub_folder)
+        os.makedirs(folder, exist_ok=True)
+        y_pred, y_target = make_eval_prediction(self.model, self.dataset, device=self.eval_device)
+        wc_viz_pred, wc_viz_target = complete_report(self.model, y_pred=y_pred, y_target=y_target)
+        pickle.dump(self.model, open(f"{folder}/model.pkl", "wb"))
+        pickle.dump(self.dataset, open(f"{folder}/dataset.pkl", "wb"))
+        pickle.dump(wc_viz_pred, open(f"{folder}/viz_pred.pkl", "wb"))
+        pickle.dump(wc_viz_target, open(f"{folder}/viz_target.pkl", "wb"))
+
+    def to_txt(self):
+        # summary = model_summary(self.model, (1, self.dataset.n_units))
+        folder = os.path.join(self.model.checkpoint_folder, "infos")
+        os.makedirs(folder, exist_ok=True)
+
+        save_str_to_file(f"{folder}/model_repr.txt", repr(self.model))
+        save_str_to_file(f"{folder}/dataset_repr.txt", repr(self.dataset))
+        save_str_to_file(f"{folder}/trainer_repr.txt", repr(self.trainer))
+        save_str_to_file(f"{folder}/trainer_state_repr.txt", repr(self.trainer.state))
+        # save_str_to_file(f"{folder}/model_summary.txt", summary)
+
+    def try_save_all(self):
+        try:
+            self.to_pickle()
+        except:
+            pass
+        try:
+            self.to_txt()
+        except:
+            pass
+
+    def start(self, trainer: nt.Trainer, **kwargs):
+        super().start(trainer, **kwargs)
+        self.try_save_all()
+
+    def close(self, trainer: nt.Trainer, **kwargs):
+        super().close(trainer, **kwargs)
+        self.try_save_all()
+
+    def __del__(self):
+        self.try_save_all()
+        super().__del__()
+
+    def extra_repr(self) -> str:
+        return f"pkl_sub_folder={self.pkl_sub_folder}, repr_sub_folder={self.repr_sub_folder}"
+
+
+class MixTrainingLearningAlgorithm(LearningAlgorithm):
+    DEFAULT_PRIORITY = nt.callbacks.BaseCallback.DEFAULT_HIGH_PRIORITY
+
+    def __init__(
+            self,
+            learning_algorithms: Optional[Iterable[LearningAlgorithm]] = None,
+            n_epochs_per_la: Optional[int] = 10,
+    ):
+        super().__init__(save_state=False)
+        self._learning_algorithms = learning_algorithms
+        self.n_epochs_per_la = n_epochs_per_la
+
+    def filter_learning_algorithms(self, callbacks: Iterable[nt.callbacks.BaseCallback]) -> List[LearningAlgorithm]:
+        la_list = [callback for callback in callbacks if isinstance(callback, LearningAlgorithm)]
+        # make sure that self is not in the list
+        la_list = [la for la in la_list if not isinstance(la, MixTrainingLearningAlgorithm)]
+        if self in la_list:
+            la_list.remove(self)
+        return la_list
+
+    def start(self, trainer: nt.Trainer, **kwargs):
+        super().start(trainer, **kwargs)
+        if self._learning_algorithms is None:
+            self._learning_algorithms = trainer.callbacks
+        self._learning_algorithms = self.filter_learning_algorithms(self._learning_algorithms)
+        for la in self._learning_algorithms:
+            la.start(trainer, **kwargs)
+
+        trainer.update_state_(
+            n_epochs=max(trainer.state.n_epochs, self.n_epochs_per_la * len(self._learning_algorithms))
         )
-        delta_weights_sign = torch.sign(delta_weights)
-        delta_weights = delta_weights_sign * torch.sqrt(torch.abs(delta_weights))
-        return delta_weights, delta_inv_corr
+
+        # remove the learning algorithms from the trainer's callbacks
+        self.add_learning_algorithms_to_trainer(trainer, **kwargs)
+
+    def remove_learning_algorithms_from_trainer(self, trainer: nt.Trainer, **kwargs):
+        callbacks_to_remove = self.filter_learning_algorithms(trainer.callbacks)
+        for la in callbacks_to_remove:
+            trainer.callbacks.remove(la)
+
+    def add_learning_algorithms_to_trainer(self, trainer: nt.Trainer, **kwargs):
+        for la in self._learning_algorithms:
+            if la not in trainer.callbacks:
+                trainer.callbacks.append(la)
+
+    def on_epoch_begin(self, trainer, **kwargs):
+        super().on_iteration_begin(trainer, **kwargs)
+        self.remove_learning_algorithms_from_trainer(trainer, **kwargs)
+        # curr_la = self._learning_algorithms[trainer.state.epoch % len(self._learning_algorithms)]
+        la_idx = (trainer.state.epoch // self.n_epochs_per_la) % len(self._learning_algorithms)
+        curr_la = self._learning_algorithms[la_idx]
+        trainer.callbacks.append(curr_la)
+        curr_la.on_epoch_begin(trainer, **kwargs)
+        p_bar = trainer.state.objects.get("p_bar", None)
+        if p_bar is not None:
+            if p_bar.postfix is None:
+                curr_postfix = {}
+            else:
+                curr_postfix = {
+                    p_str.split("=")[0].strip(): p_str.split("=")[1].strip()
+                    for p_str in p_bar.postfix.split(",")
+                    if "=" in p_str
+                }
+            curr_postfix["la"] = getattr(curr_la, "name", curr_la.__class__.__name__)
+            curr_postfix["epoch"] = f"{trainer.state.epoch + 1}/{trainer.state.n_epochs}"
+            p_bar.set_postfix(curr_postfix)
+
+    def on_epoch_end(self, trainer, **kwargs):
+        super().on_iteration_end(trainer, **kwargs)
+        self.remove_learning_algorithms_from_trainer(trainer, **kwargs)
+        self.add_learning_algorithms_to_trainer(trainer, **kwargs)
+
+    def extra_repr(self) -> str:
+        if self._learning_algorithms is None:
+            return f"learning_algorithms=None"
+        learning_algorithms = self.filter_learning_algorithms(self._learning_algorithms)
+        la_names = [getattr(la, "name", la.__class__.__name__) for la in learning_algorithms]
+        return f"learning_algorithms={la_names}, n_epochs_per_la={self.n_epochs_per_la}"
